b97192851c0ed96e2b647a6bc90f2918ad781ee0
mikeeeola <48551269+mikeeeola@users.noreply.github.com>
2022-05-14 23:22:37 +0100

    doc added

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
 const port = process.env.PORT || 5000;
 
 const start = async () => {
