9627903ba94e3a9a42fdb1a955c5c6248f17d8fd
mikeeeola <48551269+mikeeeola@users.noreply.github.com>
2022-05-15 00:07:45 +0100

    swagger docs added

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
-require("dotenv").config();
-require("express-async-errors");
-const express = require("express");
-const app = express();
+require('dotenv').config();
+require('express-async-errors');
 
-// connectDB
-const connectDB = require('./db/connect');
-
-const authenticateUser = require('./middleware/authentication')
-
-// routers
-const authRouter = require("./routes/auth");
-const jobsRouter = require("./routes/jobs");
-
-// error handler
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
-
-app.use(express.json());
-
-// extra packages
+// extra security packages
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
-// routes
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
+// routers
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust proxy', 1);
 app.use(
@@ -46,12 +39,20 @@ app.use(xss());
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// routes
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
